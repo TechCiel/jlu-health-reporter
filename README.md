d6d5df449188a703f3554567b2e9fcd07f362af4
TechCiel <i@ciel.dev>
2020-08-10 22:21:27 +0800

    new version for 3plus1 checkin

--- README.md
+++ README.md
@@ -1,49 +1,47 @@
 # JLU Health Reporter
 
-为吉林大学本科生每日健康情况申报所作的自动机器人。
+__**警告：当前版本是根据一次抓取数据写成的，尚在等待测试，极有可能工作不正常。**__
 
-以 WTFPL 授权开源。Pray for Hubei.
+为吉林大学本科生每日健康打卡所作的自动机器人。（三测温一点名版）
+
+以 WTFPL 授权开源。
 
 ## 免责声明
 
 本自动程序为个人使用开发，未经充分测试，不保证正常工作。
 
-本自动程序仅适用于 2020 年初 COVID-19 疫情期间吉林大学本科生、研究生健康情况申报，不保证按时更新。
+本自动程序适用于 2020-2021 秋季学期吉林大学本科生每日健康打卡（三测温一点名），不保证按时更新。研究生打卡是否适用未经测试。
 
-请注意，本自动程序仅会每 24 小时自动重新提交上次提交的内容，**如您的申报内容变化，请立即停止使用本程序！**
+**使用本程序自动提交打卡，你必须实际完成一日三测温，在指定时间回到寝室，并在身体状况出现异常时立刻联系校医院和辅导员。**
 
-__**如运行本程序，您理解并认可，本自动程序的一切操作均视为您本人、或由您授权的操作。**__
+__**如运行本程序，您理解并认可，本自动程序的一切操作均视为您本人进行、或由您授权的操作。本程序作者对您因使用此程序可能受到的损失、处罚以及造成的法律后果不负任何责任。**__
 
 ## 使用说明
 
 需要 Python 3 ，先 `pip3 install requests` 。
 
-运行之前先登录平台提交一次申报，务必确保信息准确。
+运行之前**先登录平台提交一次打卡**，务必确保信息准确。
 
-把文件开头的 `USERS` 中的示例用户名和密码换为自己的，支持多帐号。
+把文件开头的 `USERS` 中的示例用户名和密码换为自己的，支持多帐号但不建议过多，避免错过打卡窗口期。
 
 若为**研究生健康申报**使用，请先改变文件开头的 `TRANSACTION` 。
-
-后台模式：
-
-```
-./jlu-health-reporter.py & > reporter.log 2>&1
-```
 
 Crontab 模式：
 
 ```
-0 6 * * * /usr/bin/python3 /path/to/jlu-health-reporter.py --once >> reporter.log 2>&1
+5 7 * * * /usr/bin/python3 /path/to/jlu-health-reporter.py >> reporter.log 2>&1
+5 11 * * * /usr/bin/python3 /path/to/jlu-health-reporter.py >> reporter.log 2>&1
+5 17 * * * /usr/bin/python3 /path/to/jlu-health-reporter.py >> reporter.log 2>&1
+5 21 * * * /usr/bin/python3 /path/to/jlu-health-reporter.py >> reporter.log 2>&1
+# 5分开始避免服务器时间略有偏差导致失败
 ```
 
-手动模式：
+手动模式（请在时段内启动）：
 
 ```
-./jlu-health-reporter.py --once
+./jlu-health-reporter.py
 ```
 
 ## 联系
 
-邮箱在源代码里。
-
-欢迎开 issue 、pr ，随缘处理。
+欢迎开 issue / pr ，随缘处理。
--- jlu-health-reporter.py
+++ jlu-health-reporter.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python3
-USERS = [('zhaoyy2119', 'PASSWORD')]
-# USERNAME and USERPASS are deprecated now for multi-user support
-USERNAME = 'zhaoyy2119'
-USERPASS = 'PASSWORD'
-INTERVAL = 86400 
+USERS = [('zhangsan2119', 'PASSWORD')]
 MAX_RETRY = 30
-RETRY_INTERVAL = 20
-TRANSACTION = 'JLDX_BK_XNYQSB' # 'JLDX_YJS_XNYQSB'git
-DEBUG = 0#+1
+RETRY_INTERVAL = 10
+TRANSACTION = 'BKSMRDK' # 'YJSMRDK'
+DEBUG = 0+1
 
 import re
-from sys import argv
 import json
 from time import time, sleep
 import urllib3
@@ -21,75 +16,68 @@ from logging import debug, info, warning, error, critical
 logging.basicConfig(level=logging.INFO-10*DEBUG, format='%(asctime)s %(levelname)s %(message)s')
 warning('Started.')
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-if USERS is None: USERS = [(USERNAME, USERPASS)]
 
-while True: # main loop
-	for USERNAME, USERPASS in USERS:
-		for tries in range(0, MAX_RETRY):
-			try:
-				info('User: '+USERNAME)
-				info('Authenticating...')
-				s = requests.Session()
-				s.headers.update({'Referer':'https://ehall.jlu.edu.cn/'})
-				s.verify = False
-				
-				r = s.get('https://ehall.jlu.edu.cn/jlu_portal/login')
-				pid = re.search('(?<=name="pid" value=")[a-z0-9]{8}', r.text)[0]
-				debug('PID: '+pid)
+for USERNAME, USERPASS in USERS:
+	info('Processing {USERNAME}...')
+	for tries in range(MAX_RETRY):
+		try:
+			info('Authenticating...')
+			s = requests.Session()
+			s.headers.update({'Referer':'https://ehall.jlu.edu.cn/'})
+			s.verify = False
+			
+			r = s.get('https://ehall.jlu.edu.cn/jlu_portal/login')
+			pid = re.search('(?<=name="pid" value=")[a-z0-9]{8}', r.text)[0]
+			debug('PID: '+pid)
 
-				postPayload = {'username':USERNAME,'password':USERPASS,'pid':pid}
-				r = s.post('https://ehall.jlu.edu.cn/sso/login',data=postPayload)
+			postPayload = {'username':USERNAME,'password':USERPASS,'pid':pid}
+			r = s.post('https://ehall.jlu.edu.cn/sso/login',data=postPayload)
 
-				info('Requesting form...')
-				r = s.get('https://ehall.jlu.edu.cn/infoplus/form/'+TRANSACTION+'/start')
-				csrfToken = re.search('(?<=csrfToken" content=").{32}',r.text)[0]
-				debug('CSRF: '+csrfToken)
+			info('Requesting form...')
+			r = s.get('https://ehall.jlu.edu.cn/infoplus/form/'+TRANSACTION+'/start')
+			csrfToken = re.search('(?<=csrfToken" content=").{32}',r.text)[0]
+			debug('CSRF: '+csrfToken)
 
-				postPayload = {'idc':TRANSACTION,'csrfToken':csrfToken}
-				r = s.post('https://ehall.jlu.edu.cn/infoplus/interface/start',data=postPayload)
-				sid = re.search('(?<=form/)\\d*(?=/render)',r.text)[0]
-				debug('Step ID: '+sid)
+			postPayload = {'idc':TRANSACTION,'csrfToken':csrfToken}
+			r = s.post('https://ehall.jlu.edu.cn/infoplus/interface/start',data=postPayload)
+			sid = re.search('(?<=form/)\\d*(?=/render)',r.text)[0]
+			debug('Step ID: '+sid)
 
-				postPayload = {'stepId':sid,'csrfToken':csrfToken}
-				r = s.post('https://ehall.jlu.edu.cn/infoplus/interface/render',data=postPayload)
-				data = json.loads(r.content)['entities'][0]
-				payload_1 = data['data']
-				payload_1['fieldCNS'] = True
-				payload_1 = json.dumps(payload_1)
-				debug('DATA: '+payload_1)
-				payload_2 = ','.join(data['fields'].keys())
-				debug('FIELDS: '+payload_2)
+			postPayload = {'stepId':sid,'csrfToken':csrfToken}
+			r = s.post('https://ehall.jlu.edu.cn/infoplus/interface/render',data=postPayload)
+			data = json.loads(r.content)['entities'][0]
+			payload_1 = data['data']
+			payload_1['fieldZtw'] = '1'
+			payload_1['fieldZhongtw'] = '1'
+			payload_1['fieldWantw'] = '1'
+			payload_1 = json.dumps(payload_1)
+			debug('DATA: '+payload_1)
+			payload_2 = ','.join(data['fields'].keys())
+			debug('FIELDS: '+payload_2)
 
-				info('Submitting form...')
-				postPayload = {
-					'actionId':1,
-					'formData':payload_1,
-					'nextUsers':'{}',
-					'stepId':sid,
-					'timestamp':int(time()),
-					'boundFields':payload_2,
-					'csrfToken':csrfToken
-				}
-				r = s.post('https://ehall.jlu.edu.cn/infoplus/interface/doAction',data=postPayload)
-				debug(r.text)
+			info('Submitting form...')
+			postPayload = {
+				'actionId':1,
+				'formData':payload_1,
+				'nextUsers':'{}',
+				'stepId':sid,
+				'timestamp':int(time()),
+				'boundFields':payload_2,
+				'csrfToken':csrfToken
+			}
+			r = s.post('https://ehall.jlu.edu.cn/infoplus/interface/doAction',data=postPayload)
+			debug(r.text)
 
-				if json.loads(r.content)['ecode'] != 'SUCCEED' :
-					raise Exception('The server returned a non-successful status.')
-				
-				info('Success!')
-				break
+			if json.loads(r.content)['ecode'] != 'SUCCEED' :
+				raise Exception('The server returned a non-successful status.')
+			
+			info('Success!')
+			break
 
-			except Exception as e:
-				warning(e)
-				if tries+1 == MAX_RETRY :
-					error('Failed too many times! Skipping...')
-					break
-				error('Unknown error occured, retrying...')
-				sleep(RETRY_INTERVAL)
+		except Exception as e:
+			error(e)
+			error('Unknown error occured!')
+			sleep(RETRY_INTERVAL)
 
-	if len(argv) > 1 and argv[1] == '--once' :
-		info('Exiting...')
-		exit()
-
-	info('Waiting for next run...')
-	sleep(INTERVAL)
+info('Exiting...')
+exit()
