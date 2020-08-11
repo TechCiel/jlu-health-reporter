644c43a2093083d816d7ed7bdd965a3ab712d92e
TechCiel <i@ciel.dev>
2020-08-11 17:08:36 +0800

    intro config file to support custom fields filling

--- jlu-health-reporter.py
+++ jlu-health-reporter.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
-USERS = [('zhangsan2119', 'PASSWORD')]
+CONFIG = 'config.json'
 MAX_RETRY = 30
 RETRY_INTERVAL = 10
-TRANSACTION = 'BKSMRDK' # 'YJSMRDK'
 DEBUG = 0+1
 
 import re
@@ -16,9 +15,10 @@ from logging import debug, info, warning, error, critical
 logging.basicConfig(level=logging.INFO-10*DEBUG, format='%(asctime)s %(levelname)s %(message)s')
 warning('Started.')
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+config = json.load(open(CONFIG))
 
-for USERNAME, USERPASS in USERS:
-	info('Processing {USERNAME}...')
+for user in config['users']:
+	info('Processing '+user['username']+'...')
 	for tries in range(MAX_RETRY):
 		try:
 			info('Authenticating...')
@@ -30,15 +30,15 @@ for USERNAME, USERPASS in USERS:
 			pid = re.search('(?<=name="pid" value=")[a-z0-9]{8}', r.text)[0]
 			debug('PID: '+pid)
 
-			postPayload = {'username':USERNAME,'password':USERPASS,'pid':pid}
+			postPayload = {'username':user['username'],'password':user['password'],'pid':pid}
 			r = s.post('https://ehall.jlu.edu.cn/sso/login',data=postPayload)
 
 			info('Requesting form...')
-			r = s.get('https://ehall.jlu.edu.cn/infoplus/form/'+TRANSACTION+'/start')
+			r = s.get('https://ehall.jlu.edu.cn/infoplus/form/'+config['transaction']+'/start')
 			csrfToken = re.search('(?<=csrfToken" content=").{32}',r.text)[0]
 			debug('CSRF: '+csrfToken)
 
-			postPayload = {'idc':TRANSACTION,'csrfToken':csrfToken}
+			postPayload = {'idc':config['transaction'],'csrfToken':csrfToken}
 			r = s.post('https://ehall.jlu.edu.cn/infoplus/interface/start',data=postPayload)
 			sid = re.search('(?<=form/)\\d*(?=/render)',r.text)[0]
 			debug('Step ID: '+sid)
@@ -47,9 +47,8 @@ for USERNAME, USERPASS in USERS:
 			r = s.post('https://ehall.jlu.edu.cn/infoplus/interface/render',data=postPayload)
 			data = json.loads(r.content)['entities'][0]
 			payload_1 = data['data']
-			payload_1['fieldZtw'] = '1'
-			payload_1['fieldZhongtw'] = '1'
-			payload_1['fieldWantw'] = '1'
+			for u, v in config['fields'].items(): payload_1[u] = v
+			for u, v in user['fields'].items(): payload_1[u] = v
 			payload_1 = json.dumps(payload_1)
 			debug('DATA: '+payload_1)
 			payload_2 = ','.join(data['fields'].keys())
